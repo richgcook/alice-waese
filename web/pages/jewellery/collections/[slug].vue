5c4a844a9516836de47ad27eef39b7c697391191
Richard Cook <rich@richardsmini10.lan>
2025-10-02 14:08:28 +0100

    Collections test

--- web/pages/jewellery/collections/[slug].vue
+++ web/pages/jewellery/collections/[slug].vue
@@ -1,34 +1,52 @@
 <template>
-	<!--<VueLenis root :options="lenisOptions">-->
-		<div class="collection-layout">
-			<h1 class="collection-title">{{ data.collection.titleFull ? data.collection.titleFull : data.collection.title }}</h1>
-			<MasonryLayout :items="data.collection.jewellery" context="collections" v-if="data.collection.jewellery?.length" />
-			<div class="collection-description" v-if="data.collection.descriptionText?.length">
-				<RichText :blocks="data.collection.descriptionText" />
+	<div class="collections">
+
+		<!--<VueLenis root :options="lenisOptions">-->
+
+			<div class="collection-layout">
+				<h1 class="collection-title">{{ data.collection.titleFull ? data.collection.titleFull : data.collection.title }}</h1>
+				<MasonryLayout :items="data.collection.jewellery" context="collections" v-if="data.collection.jewellery?.length" />
+				<div class="collection-description" v-if="data.collection.descriptionText?.length">
+					<RichText :blocks="data.collection.descriptionText" />
+				</div>
+				<RandomIllustrationMark 
+					v-for="(name, index) in names"
+					:key="name"
+					:name="name"
+					:top="names === 2 ? (index === 2 ? { min: 70, max: 90 } : { min: 30, max: 50 }) : undefined"
+					:side="index === 1 ? 'left' : 'right'" 
+				/>
 			</div>
-			<RandomIllustrationMark 
-				v-for="(name, index) in names"
-				:key="name"
-				:name="name"
-				:top="names === 2 ? (index === 2 ? { min: 70, max: 90 } : { min: 30, max: 50 }) : undefined"
-				:side="index === 1 ? 'left' : 'right'" 
-			/>
-		</div>
-		<!--
-		<div class="collection-layout --clone">
-			<h1 class="collection-title">{{ data.collection.titleFull ? data.collection.titleFull : data.collection.title }}</h1>
-			<MasonryLayout :items="data.collection.jewellery" context="collections" v-if="data.collection.jewellery?.length" />
-			<div class="collection-description" v-if="data.collection.descriptionText?.length">
-				<RichText :blocks="data.collection.descriptionText" />
+
+			<!--
+			<div class="collection-layout --clone">
+				<h1 class="collection-title">{{ data.collection.titleFull ? data.collection.titleFull : data.collection.title }}</h1>
+				<MasonryLayout :items="data.collection.jewellery" context="collections" v-if="data.collection.jewellery?.length" />
+				<div class="collection-description" v-if="data.collection.descriptionText?.length">
+					<RichText :blocks="data.collection.descriptionText" />
+				</div>
 			</div>
-		</div>
-		-->
-	<!--</VueLenis>-->
+			-->
+			
+			<div class="collection-layout" v-for="collection in data.otherCollections" :key="collection._id">
+				<h1 class="collection-title">{{ collection.titleFull ? collection.titleFull : collection.title }}</h1>
+				<MasonryLayout :items="collection.jewellery" context="collections" v-if="collection.jewellery?.length" />
+				<div class="collection-description" v-if="collection.descriptionText?.length">
+					<RichText :blocks="collection.descriptionText" />
+				</div>
+			</div>
+
+		<!--</VueLenis>-->
+
+	</div>
 </template>
 
 <script setup>
 
 import { useIllustrationPoolStore } from '~/store/illustrationPool'
+
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 //import { VueLenis, useLenis } from 'lenis/vue'
  
@@ -62,6 +80,30 @@ const query = groq`{
 			${$richTextQuery},
 		},
 	}[0],
+
+	"otherCollections": *[_type == "productCollection" && _id != *[_type == "productCollection" && slug.current == $slug][0]._id] | order(orderRank) {
+    	_id, _type, slug, title, titleFull, seo {
+			${$seoQuery}
+		},
+		"jewellery": pageBuilder[] {
+			"item": product->{
+				${$productQuery},
+			},
+			mediaOverride {
+				image {
+					${$imageQuery}
+				},
+				imageHoverState {
+					${$imageQuery}
+				},
+				"video": video.asset->url,
+			},
+			settings,
+		},
+		descriptionText[] {
+			${$richTextQuery},
+		},
+  	},
 	
 }`
 
@@ -105,10 +147,59 @@ const lenisOptions = {
 
 //const lenis = useLenis()
 
+gsap.registerPlugin(ScrollTrigger)
+
+const triggers = []
+
+onMounted(() => {
+
+	gsap.utils.toArray('h1.collection-title').forEach((title, index) => {
+
+		if (index === 0) {
+			//gsap.set(title, { opacity: 1 })
+			//return
+		}
+
+		const show = () => gsap.to(title, { opacity: 1, duration: 0.3, overwrite: 'auto' })
+        const hide = () => gsap.to(title, { opacity: 0, duration: 0.25, overwrite: 'auto' })
+
+		const st = ScrollTrigger.create({
+			trigger: title,
+			start: 'top center',
+			onEnter: show,
+			onEnterBack: show,
+			onLeave: hide,
+			onLeaveBack: hide,
+			markers: true,
+        })
+
+		triggers.push(st)
+
+	})
+
+	ScrollTrigger.refresh()
+
+})
+
+onBeforeUnmount(() => {
+	triggers.forEach(t => t.kill())
+})
+
 </script>
 
 <style lang="scss" scoped>
 
+div.collections {
+	display: flex;
+	flex-flow: column nowrap;
+	row-gap: 250px;
+	row-gap: 27.78vh; 
+	position: relative;
+	@include media('phone') {
+		row-gap: 150px;
+		row-gap: 20.5vh;
+	}
+}
 div.collection-layout {
 	position: relative;
 	&.--clone {
@@ -122,15 +213,17 @@ div.collection-layout {
 }
 h1.collection-title {
 	position: absolute;
-	top: 25px;
+	top: 0;
 	left: 50%;
     transform: translateX(-50%);
 	width: calc(100% - (100px * 2));
+	padding-top: 25px;
 	z-index: 1;
 	font-size: clamp(48px, 6.9vw, 100px);
 	line-height: 1.2em;
 	letter-spacing: -0.03em;
 	text-align: center;
+	opacity: 0;
 	@include media('phone') {
 		width: calc(100% - (35px * 2));
 	}
